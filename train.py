e016a6565c50f757173e8d6834c032db64911571
조윤식 <cho.yunsig56@gmail.com>
2024-12-21 20:18:00 +0900

    매 배치마다 진행상황 프린트

--- train.py
+++ train.py
@@ -9,6 +9,5 @@ def train(model, train_loader, criterion, optimizer, device):
         loss.backward()
         optimizer.step()
         
-        if batch_idx % 10 == 0:
-            print(f'Training... [{batch_idx * len(data)}/{len(train_loader.dataset)} '
-                    f'({100. * batch_idx / len(train_loader):.0f}%)]\tLoss: {loss.item():.6f}')
+        print(f'Training... [{batch_idx * len(data)}/{len(train_loader.dataset)} '
+                f'({100. * batch_idx / len(train_loader):.0f}%)]\tLoss: {loss.item():.6f}')
